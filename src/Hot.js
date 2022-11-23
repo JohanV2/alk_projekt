62f314da911d6154d72edddd7e5ccb9026f6694a
JohanV2 <j.szadkowska@outlook.com>
2022-11-23 19:29:47 +0100

    Upvotes and downvotes work with live filtering

--- src/Hot.js
+++ src/Hot.js
@@ -15,17 +15,23 @@ export function Hot() {
 
     return (
         <div>
-            {filtered.map(meme => {
+            {memes.map(meme => {
                 return (
                     <div key={meme.id}>
                         <h2>title: {meme.title}</h2>
                         <h2>upvotes: {meme.upvotes}</h2>
+                        <h2>downvotes: {meme.downvotes}</h2>
                         <hr />
                         <button onClick={() => {
                             meme.upvotes += 1
                             onVote()
                         }}>
                             Upvote</button>
+                        <button onClick={() => {
+                            meme.downvotes += 1
+                            onVote()
+                        }}>
+                            Downvote</button>
                     </div>
                 );
             })}

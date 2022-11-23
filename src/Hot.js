e52ad9b0f7922305773e9a7af8de7450210ac963
JohanV2 <j.szadkowska@outlook.com>
2022-11-23 19:25:06 +0100

    add: upvotes test

--- src/Hot.js
+++ src/Hot.js
@@ -3,26 +3,32 @@ import { useState } from 'react'
 export function Hot() {
 
     const memesArray = require('./memes.json')
-    console.log(memesArray)
 
     const filtered = memesArray.filter(meme => {
         return (meme.upvotes - meme.downvotes > 5)
     })
 
-    const [upvotes, setUpvotes] = useState(0)
+    const [memes, setMemes] = useState(filtered)
+    function onVote() {
+        setMemes([...filtered])
+    }
 
     return (
         <div>
             {filtered.map(meme => {
                 return (
                     <div key={meme.id}>
-                        <h2>{meme.title}</h2>
-                        <h2>{meme.upvotes}</h2>
-                        <p>stan upvotów: {upvotes}</p>
-                        <button onClick={() => setUpvotes(upvotes + 1)}>Upvote</button>
+                        <h2>title: {meme.title}</h2>
+                        <h2>upvotes: {meme.upvotes}</h2>
+                        <hr />
+                        <button onClick={() => {
+                            meme.upvotes += 1
+                            onVote()
+                        }}>
+                            Upvote</button>
                     </div>
                 );
             })}
         </div>
-    );
+    )
 }

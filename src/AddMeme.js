8263f23467cf526dfd9925a19a3e6aa23df36855
JohanV2 <j.szadkowska@outlook.com>
2022-12-05 13:52:56 +0100

    add: redirect to addmeme screen

--- src/AddMeme.js
+++ src/AddMeme.js
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export function AddMeme(props) {
     console.log(props.test)
 
+    let navigate = useNavigate();
+    const routeChange = () => {
+        navigate("/addmeme");
+    }
+
     return (
         <div>
-            <button type="button" onClick={props.test}> Add new</button>
+            {/* <button type="button" onClick={props.test}> Add new</button> */}
+            <button type="button" onClick={routeChange}> Add new</button>
         </div >
     );
 };

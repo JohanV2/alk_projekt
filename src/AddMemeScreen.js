ecd6d453a02c8af337e7c79c6df0d6881ebe3ff0
JohanV2 <j.szadkowska@outlook.com>
2022-12-06 13:23:47 +0100

    add: add meme form forms

--- src/AddMemeScreen.js
+++ src/AddMemeScreen.js
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 export function AddMemeScreen(props) {
-    let test = () => {
-        // props.memesArray = [...props.memesArray, { title: "Hello", upvotes: 1, downvotes: 1 }]
-        // console.log(props.memesArray)
-        props.memesArray.push({ title: "Hello", upvotes: 1, downvotes: 1 })
-        console.log(props.memesArray)
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.memesArray.push({ id: props.memesArray.length + 1, title: inputs.title, upvotes: 0, downvotes: 0, img: inputs.img })
+        setInputs([])
     }
 
     return (
-        <div>
-            <button type="button" onClick={props.test}> Add test</button>
-            <button type="button" onClick={test}> Add to array</button>
-        </div >
-    );
-};
+        <form onSubmit={handleSubmit}>
+            <label>Enter meme title:
+                <input
+                    type="text"
+                    name="title"
+                    value={inputs.title || ""}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Enter image url:
+                <input
+                    type="text"
+                    name="img"
+                    value={inputs.img || ""}
+                    onChange={handleChange}
+                />
+            </label>
+            <input type="submit" />
+        </form>
+    )
+}

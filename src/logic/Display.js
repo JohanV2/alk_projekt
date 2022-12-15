6454ea380fbaf5626c5b63395255937eee295db0
JohanV2 <j.szadkowska@outlook.com>
2022-12-15 17:52:52 +0100

    add: buttons color switch on vote

--- src/logic/Display.js
+++ src/logic/Display.js
@@ -1,33 +1,45 @@
 import { AddMeme } from "./AddMeme";
-
 import "./Display.scss"
 
 export function Display(props) {
-
+    const buttonColorActive = "rgb(59, 92, 255)";
+    const buttonColorInactive = "rgb(168, 168, 168)";
     return (
         <div className="memes" >
             {
                 props.memesArray.map(meme => {
-                    const voteClick = (vote, antagonist) => {
+                    const voteClick = (vote, antagonist, buttonType) => {
                         if (props.GODMODE === false) {
-                            checkUserVotes(vote, antagonist)
+                            checkUserVotes(vote, antagonist, buttonType)
                         } else {
                             godModeVotes(vote)
                         }
                     }
-                    const checkUserVotes = (vote, antagonist) => {
+                    const checkUserVotes = (vote, antagonist, buttonType) => {
+                        let antagonistButtonType = ""
+                        if (buttonType === "u") {
+                            antagonistButtonType = "d"
+                        } else if (buttonType === "d") {
+                            antagonistButtonType = "u"
+                        } else {
+                            console.log("wrong button type")
+                        }
                         const indexVote = vote.indexOf(props.userName)
                         const indexAntagonist = antagonist.indexOf(props.userName)
                         if (indexVote > -1 && !indexAntagonist > -1) {
                             vote.splice(indexVote, 1)
+                            document.getElementById(meme.id + buttonType).style.backgroundColor = buttonColorInactive
                         }
                         else if (indexAntagonist > -1) {
                             vote.push(props.userName)
+                            document.getElementById(meme.id + buttonType).style.backgroundColor = buttonColorActive
 
                             antagonist.splice(indexAntagonist, 1)
+                            document.getElementById(meme.id + antagonistButtonType).style.backgroundColor = buttonColorInactive
                         }
                         else {
                             vote.push(props.userName)
+                            document.getElementById(meme.id + buttonType).style.backgroundColor = buttonColorActive
                         }
                         props.onVote()
                     }
@@ -42,12 +54,12 @@ export function Display(props) {
                                 <h2>{meme.title}</h2>
                                 <img src={meme.img} loading="lazy" alt={meme.title} />
                                 <div className="buttons">
-                                    <button onClick={() => {
-                                        voteClick(meme.upvotes, meme.downvotes)
+                                    <button id={meme.id + "u"} onClick={() => {
+                                        voteClick(meme.upvotes, meme.downvotes, "u")
                                     }}>
                                         🢁 {meme.upvotes.length}</button>
-                                    <button onClick={() => {
-                                        voteClick(meme.downvotes, meme.upvotes)
+                                    <button id={meme.id + "d"} onClick={() => {
+                                        voteClick(meme.downvotes, meme.upvotes, "d")
                                     }}>
                                         🢃 {meme.downvotes.length}</button>
                                 </div>
